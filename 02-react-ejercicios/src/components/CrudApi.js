1a3d53cec5707e3fcf07d6d60e43645ba2088f9f
Carlos Emmanuel Méndez Calderón <mcalderon.carlos@gmail.com>
2022-12-14 21:02:43 -0600

    Continuar en https://www.youtube.com/watch?v=KnncoyOGNqU&list=PLvq-jIkSeTUZ5XcUw8fJPTBKEHEKPMTKk&index=37

---
+++ 02-react-ejercicios/src/components/CrudApi.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect } from "react";
+import { helpHttp } from "../helpers/helpHttp";
+import CrudForm from "./CrudForm";
+import CrudTable from "./CrudTable";
+import Loader from "./Loader";
+import Message from "./Message";
+
+const CrudApi = () => {
+  const [db, setDb] = useState(null);
+  const [dataToEdit, setDataToEdit] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  //   let api = helpHttp();
+  let url = "http://localhost:5000/santos";
+
+  useEffect(() => {
+    setLoading(true);
+    helpHttp()
+      .get(url)
+      .then((res) => {
+        //   console.log(res);
+        if (!res.err) {
+          setDb(res);
+          setError(null);
+        } else {
+          setDb(null);
+          setError(res);
+        }
+
+        setLoading();
+      });
+  }, [url]);
+
+  const createData = (data) => {
+    data.id = Date.now();
+    setDb([...db, data]);
+  };
+
+  const updateData = (data) => {
+    let newData = db.map((el) => (el.id === data.id ? data : el));
+    setDb(newData);
+  };
+
+  const deleteData = ({ id, name, constellation }) => {
+    let isDelete = window.confirm(
+      `¿Estás seguro de eliminar el registro ${name} ${constellation}?`
+    );
+
+    if (isDelete) {
+      let newData = db.filter((el) => el.id !== id);
+      setDb(newData);
+    } else {
+      return;
+    }
+  };
+
+  return (
+    <div>
+      <h2>CRUD API</h2>
+      <article className="grid-1-2">
+        <CrudForm
+          createData={createData}
+          updateData={updateData}
+          dataToEdit={dataToEdit}
+          setDataToEdit={setDataToEdit}
+        />
+        {loading && <Loader />}
+        {error && (
+          <Message
+            msg={`Error ${error.status}: ${error.statusText}`}
+            bgColor={"#dc3545"}
+          />
+        )}
+        {db && (
+          <CrudTable
+            data={db}
+            setDataToEdit={setDataToEdit}
+            deleteData={deleteData}
+          />
+        )}
+      </article>
+    </div>
+  );
+};
+
+export default CrudApi;
