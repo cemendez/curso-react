63204858ad85d3a704bc90d50850da57637fff94
Carlos Emmanuel Méndez Calderón <mcalderon.carlos@gmail.com>
2022-12-15 13:33:48 -0600

    https://www.youtube.com/watch?v=stlOav6tolo&list=PLvq-jIkSeTUZ5XcUw8fJPTBKEHEKPMTKk&index=37

--- 02-react-ejercicios/src/components/CrudApi.js
+++ 02-react-ejercicios/src/components/CrudApi.js
@@ -11,7 +11,7 @@ const CrudApi = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  //   let api = helpHttp();
+  let api = helpHttp();
   let url = "http://localhost:5000/santos";
 
   useEffect(() => {
@@ -34,12 +34,39 @@ const CrudApi = () => {
 
   const createData = (data) => {
     data.id = Date.now();
-    setDb([...db, data]);
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.post(url, options).then((res) => {
+      //   console.log(res);
+      if (!res.err) {
+        setDb([...db, data]);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const updateData = (data) => {
-    let newData = db.map((el) => (el.id === data.id ? data : el));
-    setDb(newData);
+    let endpoint = `${url}/${data.id}`;
+
+    let options = {
+      body: data,
+      headers: { "content-type": "application/json" },
+    };
+
+    api.put(endpoint, options).then((res) => {
+      //   console.log(res);
+      if (!res.err) {
+        let newData = db.map((el) => (el.id === data.id ? data : el));
+        setDb(newData);
+      } else {
+        setError(res);
+      }
+    });
   };
 
   const deleteData = ({ id, name, constellation }) => {
@@ -48,8 +75,19 @@ const CrudApi = () => {
     );
 
     if (isDelete) {
-      let newData = db.filter((el) => el.id !== id);
-      setDb(newData);
+      let endpoint = `${url}/${id}`;
+      let options = {
+        headers: { "content-type": "application/json" },
+      };
+
+      api.del(endpoint, options).then((res) => {
+        if (!res.err) {
+          let newData = db.filter((el) => el.id !== id);
+          setDb(newData);
+        } else {
+          setError(res);
+        }
+      });
     } else {
       return;
     }
---
+++ 02-react-ejercicios/src/components/SongSearch.js
@@ -0,0 +1,9 @@
+const SongSearch = () => {
+  return (
+    <div>
+      <h2>SongSearch</h2>
+    </div>
+  );
+};
+
+export default SongSearch;
