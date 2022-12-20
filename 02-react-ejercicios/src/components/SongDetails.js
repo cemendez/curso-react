affb87abd6ed53351b4439100b3ca3dbc7fb4e10
Carlos Emmanuel Méndez Calderón <mcalderon.carlos@gmail.com>
2022-12-19 20:38:36 -0600

    Continuar en https://www.youtube.com/watch?v=l6S90ah48kk&list=PLvq-jIkSeTUZ5XcUw8fJPTBKEHEKPMTKk&index=42

--- 02-react-ejercicios/src/components/SongDetails.js
+++ 02-react-ejercicios/src/components/SongDetails.js
@@ -7,19 +7,19 @@ const SongDetails = ({ search, lyric, bio }) => {
 
   return (
     <>
-      {lyric.error || lyric.name === "AbortError" ? (
+      {lyric.error || lyric.err || lyric.name === "AbortError" ? (
         <Message
-          msg={`Error: no existe la canción ${search.song}`}
+          msg={`Error: no existe la canción <em>${search.song}</em>`}
           bgColor="#dc3545"
         />
       ) : (
-        <SongLyric />
+        <SongLyric title={search.song} lyrics={lyric.lyrics} />
       )}
       {bio.artists ? (
-        <SongArtist />
+        <SongArtist artist={bio.artists[0]} />
       ) : (
         <Message
-          msg={`Error: no existe el intérprete ${search.artist}`}
+          msg={`Error: no existe el intérprete <em>${search.artist}</em>`}
           bgColor="#dc3545"
         />
       )}
