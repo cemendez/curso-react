133449b2c239411eea0560116d9f3bf72cf71502
Carlos Emmanuel Méndez Calderón <mcalderon.carlos@gmail.com>
2022-12-20 17:09:10 -0600

    Continuar en https://www.youtube.com/watch?v=JYFMc_S6kWc&list=PLvq-jIkSeTUZ5XcUw8fJPTBKEHEKPMTKk&index=45

---
+++ 02-react-ejercicios/src/components/SelectsAnidados.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import SelectList from "./SelectList";
+
+const SelectsAnidados = () => {
+  const [state, setState] = useState("");
+  const [town, setTown] = useState("");
+  const [suburb, setSuburb] = useState("");
+
+  const TOKEN = "47e0273a-5bb8-4a9d-a327-20d1bf80c405";
+
+  return (
+    <div>
+      <h2>Selects Anidados</h2>
+      <h3>México</h3>
+      <SelectList
+        title="estado"
+        url={`https://api.copomex.com/query/get_estados?token=${TOKEN}`}
+        handleChange={(e) => {
+          setState(e.target.value);
+        }}
+      />
+      {state && (
+        <SelectList
+          title="municipios"
+          url={`https://api.copomex.com/query/get_municipio_por_estado/${state}?token=${TOKEN}`}
+          handleChange={(e) => {
+            setTown(e.target.value);
+          }}
+        />
+      )}
+      {town && (
+        <SelectList
+          title="colonia"
+          url={`https://api.copomex.com/query/get_colonia_por_municipio/${town}?token=${TOKEN}`}
+          handleChange={(e) => {
+            setSuburb(e.target.value);
+          }}
+        />
+      )}
+      <pre>
+        <code>
+          {state} - {town} - {suburb}
+        </code>
+      </pre>
+    </div>
+  );
+};
+
+export default SelectsAnidados;
