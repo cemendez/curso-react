6b1c1364cca1937033ec8b80f3c13730c505249d
Carlos Emmanuel Méndez Calderón <mcalderon.carlos@gmail.com>
2022-12-21 16:57:46 -0600

    Continuar en https://www.youtube.com/watch?v=nRWk8pAN74c&list=PLvq-jIkSeTUZ5XcUw8fJPTBKEHEKPMTKk&index=52

---
+++ 02-react-ejercicios/src/components/Modals.js
@@ -0,0 +1,40 @@
+import { useModal } from "../hooks/useModal";
+import ContactForm from "./ContactForm";
+import Modal from "./Modal";
+
+const Modals = () => {
+  const [isOpenModal1, openModal1, closeModal1] = useModal(false);
+  const [isOpenModal2, openModal2, closeModal2] = useModal(false);
+  const [isOpenModal3, openModal3, closeModal3] = useModal(false);
+
+  return (
+    <div>
+      <h2>Modales</h2>
+
+      <button onClick={openModal1}>Modal 1</button>
+      <Modal isOpen={isOpenModal1} closeModal={closeModal1}>
+        <h3>Modal 1</h3>
+        <p>Hola, este es el contenido de mi modal 1</p>
+        <img src="https://placeimg.com/400/400/people" alt="Personas" />
+      </Modal>
+
+      <button onClick={openModal2}>Modal 2</button>
+      <Modal isOpen={isOpenModal2} closeModal={closeModal2}>
+        <h3>Modal 2</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Perspiciatis, similique cumque ipsum temporibus consectetur quos
+          maiores dolorem, tempora numquam, architecto modi hic. Consectetur
+          harum facilis iure, voluptatibus pariatur dignissimos cum.
+        </p>
+        <img src="https://placeimg.com/400/400/nature" alt="Nature" />
+      </Modal>
+      <button onClick={openModal3}>Modal Contacto</button>
+      <Modal isOpen={isOpenModal3} closeModal={closeModal3}>
+        <ContactForm />
+      </Modal>
+    </div>
+  );
+};
+
+export default Modals;
